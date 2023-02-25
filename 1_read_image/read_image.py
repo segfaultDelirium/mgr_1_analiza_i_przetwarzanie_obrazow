338c7f3b2eba9d116b4aabd121e75e4d25c22bd4
astrix <example@email.com>
2023-02-25 08:07:20 +0100

    add files

---
+++ 1_read_image/read_image.py
@@ -0,0 +1,15 @@
+import cv2 as cv2
+import numpy as np
+import os
+
+print(os.getcwd())
+
+# # img = cv.imread('../chosen_death.png')
+# img = cv.imread('../samples/korvin_bingo2.jpg')
+# img = cv.imread('../samples/chosen_death.png')
+img = cv2.imread('../samples/wallpaper.jpg')
+
+cv2.imshow('cat', img)
+cv2.waitKey(0)
+
+print(np.arange(1, 10))
---
+++ 2_read_video/read_video.py
@@ -0,0 +1,15 @@
+import cv2 as cv
+
+
+capture = cv.VideoCapture('../samples/audi_a4.mp4')
+
+while True:
+    isTrue, frame = capture.read()
+    cv.imshow('Video', frame)
+
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
+
