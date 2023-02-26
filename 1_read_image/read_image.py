93bc2093886485228cad4534d76d75d41f51c262
astrix <example@email.com>
2023-02-26 11:43:08 +0100

    add more files

--- 1_read_image/read_image.py
+++ 1_read_image/read_image.py
@@ -1,4 +1,4 @@
-import cv2 as cv2
+import cv2
 import numpy as np
 import os
 
@@ -7,9 +7,11 @@ print(os.getcwd())
 # # img = cv.imread('../chosen_death.png')
 # img = cv.imread('../samples/korvin_bingo2.jpg')
 # img = cv.imread('../samples/chosen_death.png')
-img = cv2.imread('../samples/wallpaper.jpg')
+img = cv2.imread("../samples/wallpaper.jpg")
 
-cv2.imshow('cat', img)
+cv2.imshow("cat", img)
 cv2.waitKey(0)
 
 print(np.arange(1, 10))
+
+print(os.get_exec_path())
